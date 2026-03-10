907c0a5d5147b3053e8dbf09e29b478dd2c9a07c
Riccio <166852556+Ricciow@users.noreply.github.com>
2026-03-10 17:23:29 -0300

    feat: add Crazy Carmine Conosseur achievement

---
+++ src/main/kotlin/cloud/glitchdev/rfu/achievement/achievements/CrazyCarmineConosseurAchievement.kt
@@ -0,0 +1,27 @@
+package cloud.glitchdev.rfu.achievement.achievements
+
+import cloud.glitchdev.rfu.achievement.Achievement
+import cloud.glitchdev.rfu.achievement.AchievementCategory
+import cloud.glitchdev.rfu.achievement.AchievementDifficulty
+import cloud.glitchdev.rfu.achievement.AchievementType
+import cloud.glitchdev.rfu.achievement.BaseAchievement
+import cloud.glitchdev.rfu.constants.Dyes
+import cloud.glitchdev.rfu.events.managers.DropEvents.registerDyeDropEvent
+
+@Achievement
+object CrazyCarmineConosseurAchievement : BaseAchievement() {
+    override val id: String = "crazy_carmine_conosseur"
+    override val name: String = "Crazy Carmine Conosseur"
+    override val description: String = "Drop a carmine dye"
+    override val type: AchievementType = AchievementType.NORMAL
+    override val difficulty: AchievementDifficulty = AchievementDifficulty.IMPOSSIBLE
+    override val category: AchievementCategory = AchievementCategory.ISLE
+
+    override fun setupListeners() {
+        registerDyeDropEvent { dyeDrop, _ ->
+            if(dyeDrop == Dyes.CARMINE) {
+                complete()
+            }
+        }
+    }
+}
