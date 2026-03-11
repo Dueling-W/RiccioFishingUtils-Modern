920bcb888a444b9eb5ef21435f1b0670904fec91
Riccio <166852556+Ricciow@users.noreply.github.com>
2026-03-11 00:34:20 -0300

    feat: add lucky day achievement

---
+++ src/main/kotlin/cloud/glitchdev/rfu/achievement/achievements/isle/ItsMyLuckyDayAchievement.kt
@@ -0,0 +1,25 @@
+package cloud.glitchdev.rfu.achievement.achievements.isle
+
+import cloud.glitchdev.rfu.achievement.AchievementCategory
+import cloud.glitchdev.rfu.achievement.AchievementDifficulty
+import cloud.glitchdev.rfu.achievement.AchievementType
+import cloud.glitchdev.rfu.achievement.BaseAchievement
+import cloud.glitchdev.rfu.constants.RareDrops
+import cloud.glitchdev.rfu.events.managers.DropEvents.registerRareDropEvent
+
+object ItsMyLuckyDayAchievement : BaseAchievement() {
+    override val id: String = "its_my_lucky_day"
+    override val name: String = "Its my Lucky Day!"
+    override val description: String = "Drop a Radioactive Vial"
+    override val type: AchievementType = AchievementType.NORMAL
+    override val difficulty: AchievementDifficulty = AchievementDifficulty.MEDIUM
+    override val category: AchievementCategory = AchievementCategory.ISLE
+
+    override fun setupListeners() {
+        activeListeners.add(registerRareDropEvent { drop, _ ->
+            if(drop == RareDrops.RADIOACTIVE_VIAL) {
+                complete()
+            }
+        })
+    }
+}
