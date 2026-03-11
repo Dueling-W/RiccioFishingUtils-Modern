d4999f925a1f7b79517f5b03c5989234e23fc5d7
Riccio <166852556+Ricciow@users.noreply.github.com>
2026-03-11 11:53:46 -0300

    feat: add minmaxing achievement

---
+++ src/main/kotlin/cloud/glitchdev/rfu/achievement/achievements/general/MinMaxingAchievement.kt
@@ -0,0 +1,59 @@
+package cloud.glitchdev.rfu.achievement.achievements.general
+
+import cloud.glitchdev.rfu.RiccioFishingUtils.mc
+import cloud.glitchdev.rfu.achievement.Achievement
+import cloud.glitchdev.rfu.achievement.AchievementCategory
+import cloud.glitchdev.rfu.achievement.AchievementDifficulty
+import cloud.glitchdev.rfu.achievement.AchievementType
+import cloud.glitchdev.rfu.achievement.BaseAchievement
+import cloud.glitchdev.rfu.events.managers.TickEvents.registerTickEvent
+import gg.essential.universal.utils.toUnformattedString
+import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.contents.PlainTextContents
+import net.minecraft.world.entity.EquipmentSlot
+
+@Achievement
+object MinMaxingAchievement : BaseAchievement() {
+    override val id: String = "min_maxing"
+    override val name: String = "MinMaxing"
+    override val description: String = "Hold a 10 Starred Hellfire rod while wearing full 10 Starred Magma Lord."
+    override val type: AchievementType = AchievementType.NORMAL
+    override val difficulty: AchievementDifficulty = AchievementDifficulty.HARD
+    override val category: AchievementCategory = AchievementCategory.GENERAL
+
+    private val armorSlots = arrayOf(EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET)
+
+    override fun setupListeners() {
+        activeListeners.add(registerTickEvent(interval = 100) {
+            val player = mc.player ?: return@registerTickEvent
+
+            val mainHand = player.mainHandItem.customName ?: return@registerTickEvent
+            if (!mainHand.isValidTarget("Hellfire Rod")) return@registerTickEvent
+
+            val hasFullSet = armorSlots.all { slot ->
+                val armorName = player.getItemBySlot(slot).customName ?: return@all false
+                armorName.isValidTarget("Magma Lord")
+            }
+
+            if (hasFullSet) {
+                complete()
+            }
+        })
+    }
+
+    private const val STAR_COLOR = 0xFF55FF
+    private const val STAR_STRING = "✪✪✪✪✪"
+
+    private fun Component.isValidTarget(expectedName: String): Boolean {
+        return this.toUnformattedString().contains(expectedName) && this.is10Starred()
+    }
+
+    private fun Component.is10Starred(): Boolean {
+        return this.siblings.any { part ->
+            val contents = part.contents
+            contents is PlainTextContents.LiteralContents &&
+                    contents.text == STAR_STRING &&
+                    part.style.color?.value == STAR_COLOR
+        }
+    }
+}
