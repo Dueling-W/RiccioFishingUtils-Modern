9c5d304d010abdbbe53091a0cf874f07dd03024d
Riccio <166852556+Ricciow@users.noreply.github.com>
2026-03-11 00:28:11 -0300

    feat: add failure expected achievement

---
+++ src/main/kotlin/cloud/glitchdev/rfu/achievement/achievements/isle/FailureIsExpected.kt
@@ -0,0 +1,27 @@
+package cloud.glitchdev.rfu.achievement.achievements.isle
+
+import cloud.glitchdev.rfu.achievement.Achievement
+import cloud.glitchdev.rfu.achievement.AchievementCategory
+import cloud.glitchdev.rfu.achievement.AchievementDifficulty
+import cloud.glitchdev.rfu.achievement.AchievementType
+import cloud.glitchdev.rfu.achievement.BaseAchievement
+import cloud.glitchdev.rfu.events.managers.ChatEvents.registerGameEvent
+import cloud.glitchdev.rfu.utils.dsl.toExactRegex
+
+@Achievement
+object FailureIsExpected : BaseAchievement() {
+    override val id: String = "flask_thief"
+    override val name: String = "Failure is expected..."
+    override val description: String = "Die to jawbus"
+    override val type: AchievementType = AchievementType.SECRET
+    override val difficulty: AchievementDifficulty = AchievementDifficulty.EASY
+    override val category: AchievementCategory = AchievementCategory.ISLE
+
+    val DEATH_REGEX = """ ☠ You were killed by Lord Jawbus.""".toExactRegex()
+
+    override fun setupListeners() {
+        activeListeners.add(registerGameEvent(DEATH_REGEX) { _, _, _ ->
+            complete()
+        })
+    }
+}
