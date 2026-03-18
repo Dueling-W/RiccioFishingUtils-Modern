93540ea943dcd2a31ac163bf6decf1a591d63569
Riccio <riccio.fernando@gmail.com>
2026-03-18 12:26:40 -0300

    refactor: move the compact logic

---
+++ src/main/kotlin/cloud/glitchdev/rfu/utils/dsl/Long.kt
@@ -0,0 +1,10 @@
+package cloud.glitchdev.rfu.utils.dsl
+
+fun Long.compact(): String {
+    return when {
+        this >= 1_000_000_000L -> "${String.format("%.1f", this / 1_000_000_000.0)}B"
+        this >= 1_000_000L -> "${String.format("%.1f", this / 1_000_000.0)}M"
+        this >= 10_000L -> "${String.format("%.1f", this / 1_000.0)}k"
+        else -> this.toString()
+    }.replace(".0", "")
+}
