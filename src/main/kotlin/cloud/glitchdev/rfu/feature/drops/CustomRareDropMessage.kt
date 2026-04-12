df8f6eef887e9abb5a97d9abec323f17dc2f3dbc
Riccio <166852556+Ricciow@users.noreply.github.com>
2026-04-12 02:11:29 -0300

    fix: remove unused import

--- src/main/kotlin/cloud/glitchdev/rfu/feature/drops/CustomRareDropMessage.kt
+++ src/main/kotlin/cloud/glitchdev/rfu/feature/drops/CustomRareDropMessage.kt
@@ -9,7 +9,6 @@ import cloud.glitchdev.rfu.data.drops.DropRecord
 import cloud.glitchdev.rfu.utils.Chat.sendMessage
 import cloud.glitchdev.rfu.utils.Chat.sendPartyMessage
 import cloud.glitchdev.rfu.utils.dsl.formatTemplate
-import cloud.glitchdev.rfu.utils.dsl.toMcCodes
 import cloud.glitchdev.rfu.utils.dsl.toReadableString
 import gg.essential.universal.utils.toUnformattedString
 import net.minecraft.network.chat.Component
